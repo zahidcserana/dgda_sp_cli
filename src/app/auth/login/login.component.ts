490af5ae7c274d942543123ab72d0bb459e7a6ce
zahidcserana <zahid1004048@gmail.com>
2019-06-13 17:52:18 +0600

    auth

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -20,6 +20,9 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
+    if(this.authService.authenticated){
+      this.router.navigate(['/home']);
+    }
     this.getSettings();
   }
 
