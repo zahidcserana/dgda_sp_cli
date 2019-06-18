8bfc0d0bd13631afb4eabb4a8347b3a6e3e68d1f
zahidcserana <zahid1004048@gmail.com>
2019-06-18 19:12:21 +0600

    purchase form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,55 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Helpers} from '../helpers';
 import {ScriptLoaderService} from '../_services/script-loader.service';
-import { Router } from '@angular/router';
+import {Router} from '@angular/router';
 import * as $ from 'jquery';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor( private _script: ScriptLoaderService,
-    private router: Router) {
-      this.getSettings();
-      $('body').attr('class', 'no-skin');
-  }
+    constructor(private _script: ScriptLoaderService,
+                private router: Router) {
+        this.getSettings();
+        $('body').attr('class', 'no-skin');
+    }
 
-  ngOnInit() {
-    
-    
-  }
-  getSettings() {
-    Helpers.loadStyles('head', 'assets/css/bootstrap.min.css');
-    Helpers.loadStyles('head', 'assets/font-awesome/4.5.0/css/font-awesome.min.css');
-    Helpers.loadStyles('head', 'assets/css/fonts.googleapis.com.css');
-    Helpers.loadStyles('head', 'assets/css/ace.min.css');
-    Helpers.loadStyles('head', 'assets/css/ace-part2.min.css');
-    Helpers.loadStyles('head', 'assets/css/ace-skins.min.css');
-    Helpers.loadStyles('head', 'assets/css/ace-rtl.min.css');
-    Helpers.loadStyles('head', 'assets/css/ace-ie.min.css');
+    ngOnInit() {
 
-    this._script.loadScripts('body', [
-      'assets/js/ace-extra.min.js',
-      'assets/js/html5shiv.min.js',
-      'assets/js/respond.min.js',
-      'assets/js/jquery-2.1.4.min.js',
-      'assets/js/jquery-1.11.3.min.js',
-      'assets/js/bootstrap.min.js',
-      'assets/js/excanvas.min.js',
-      'assets/js/jquery-ui.custom.min.js',
-      'assets/js/jquery.ui.touch-punch.min.js',
-      'assets/js/jquery.easypiechart.min.js',
-      'assets/js/jquery.sparkline.index.min.js',
-      'assets/js/jquery.flot.min.js',
-      'assets/js/jquery.flot.pie.min.js',
-      'assets/js/jquery.flot.resize.min.js',
-      'assets/js/ace-elements.min.js',
-      'assets/js/ace.min.js',
-    ])
-        .then(result => {
-          //Helpers.setLoading(false);
-        });
-  }
+
+    }
+
+    getSettings() {
+        Helpers.loadStyles('head', 'assets/css/bootstrap.min.css');
+        Helpers.loadStyles('head', 'assets/font-awesome/4.5.0/css/font-awesome.min.css');
+        Helpers.loadStyles('head', 'assets/css/fonts.googleapis.com.css');
+        Helpers.loadStyles('head', 'assets/css/ace.min.css');
+        Helpers.loadStyles('head', 'assets/css/ace-part2.min.css');
+        Helpers.loadStyles('head', 'assets/css/ace-skins.min.css');
+        Helpers.loadStyles('head', 'assets/css/ace-rtl.min.css');
+        Helpers.loadStyles('head', 'assets/css/ace-ie.min.css');
+
+        this._script.loadScripts('body', [
+            'assets/js/ace-extra.min.js',
+            'assets/js/html5shiv.min.js',
+            'assets/js/respond.min.js',
+            'assets/js/jquery-2.1.4.min.js',
+            'assets/js/jquery-1.11.3.min.js',
+            'assets/js/bootstrap.min.js',
+            'assets/js/excanvas.min.js',
+            'assets/js/jquery-ui.custom.min.js',
+            'assets/js/jquery.ui.touch-punch.min.js',
+            'assets/js/jquery.easypiechart.min.js',
+            'assets/js/jquery.sparkline.index.min.js',
+            'assets/js/jquery.flot.min.js',
+            'assets/js/jquery.flot.pie.min.js',
+            'assets/js/jquery.flot.resize.min.js',
+            'assets/js/ace-elements.min.js',
+            'assets/js/ace.min.js',
+        ])
+            .then(result => {
+                // Helpers.setLoading(false);
+            });
+    }
 }
--- src/app/home/purchase/purchase.component.ts
+++ src/app/home/purchase/purchase.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Helpers} from '../../helpers';
+import {ScriptLoaderService} from '../../_services/script-loader.service';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-purchase',
-  templateUrl: './purchase.component.html',
-  styleUrls: ['./purchase.component.css']
+    selector: 'app-purchase',
+    templateUrl: './purchase.component.html',
+    styleUrls: ['./purchase.component.css']
 })
 export class PurchaseComponent implements OnInit {
 
-  constructor() { }
+    constructor(private _script: ScriptLoaderService,
+                private router: Router) {
+        this.getSettings();
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    getSettings() {
+        Helpers.loadStyles('head', 'assets/css/jquery-ui.custom.min.css');
+        Helpers.loadStyles('head', 'assets/css/chosen.min.css');
+        Helpers.loadStyles('head', 'assets/css/bootstrap-datepicker3.min.css');
+        Helpers.loadStyles('head', 'assets/css/bootstrap-timepicker.min.css');
+        Helpers.loadStyles('head', 'assets/css/daterangepicker.min.css');
+        Helpers.loadStyles('head', 'assets/css/bootstrap-datetimepicker.min.css');
+        Helpers.loadStyles('head', 'assets/css/bootstrap-colorpicker.min.css');
+
+        this._script.loadScripts('body', [
+            'assets/js/chosen.jquery.min.js',
+            'assets/js/spinbox.min.js',
+            'assets/js/bootstrap-datepicker.min.js',
+            'assets/js/bootstrap-timepicker.min.js',
+            'assets/js/moment.min.js',
+            'assets/js/daterangepicker.min.js',
+            'assets/js/bootstrap-datetimepicker.min.js',
+            'assets/js/bootstrap-colorpicker.min.js',
+            'assets/js/jquery.knob.min.js',
+            'assets/js/autosize.min.js',
+            'assets/js/jquery.inputlimiter.min.js',
+            'assets/js/jquery.maskedinput.min.js',
+            'assets/js/bootstrap-tag.min.js',
+        ])
+            .then(result => {
+                // Helpers.setLoading(false);
+            });
+    }
 }
