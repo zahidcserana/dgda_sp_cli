5671fce259ddb933aef71e202a0cdc32bc5df6e7
zahidcserana <zahid1004048@gmail.com>
2019-06-25 19:15:22 +0600

    service

---
+++ src/app/home/service/home-resolve.service.ts
@@ -0,0 +1,28 @@
+
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot} from '@angular/router';
+import { HomeService } from './home.service';
+
+
+
+@Injectable()
+export class HomeResolveService implements Resolve<any> {
+
+    constructor(private service: HomeService) {
+    }
+
+    resolve(router: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+
+        switch (router.routeConfig.path) {
+            case 'purchase':
+                return this.service.getCompanies();
+        
+        }
+    }
+
+    private getId(router) {
+        let id = router.parent.params.product_id;
+        id = id ? id : router.parent.parent.parent.params.product_id;
+        return id;
+    }
+}
---
+++ src/app/home/service/home.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@angular/core";
+import { HttpService } from '../../modules/http-with-injector/http.service';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class HomeService {
+
+    constructor(private http: HttpService){
+    
+    }
+
+    getCompanies() {
+        return this.http.get('companies').pipe(map(res => res));
+    }
+}
