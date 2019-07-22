b5420cbf98c19f52bd81f56830166a9c459e769d
zahidcserana <zahid1004048@gmail.com>
2019-07-22 12:32:28 +0600

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   // api_url: 'http://54.214.203.243:91/',
-  // api_url: 'http://103.23.41.189:99/',
   api_url: 'http://dgdasp.local/',
   protocal: 'http://',
   storeHoshName: 'localhost:4200',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,6 @@
 export const environment = {
   production: false,
   // api_url: 'http://54.214.203.243:91/',
-  // api_url: 'http://103.23.41.189:99/',
   api_url: 'http://dgdasp.local/',
   protocal: 'http://',
   storeHoshName: 'localhost:4200',
